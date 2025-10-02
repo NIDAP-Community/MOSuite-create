4353589ff7b67eebe5314fc2edec49fbacc138a0
Conor Mohan <conor.mohan@codeocean.com>
2025-10-02 12:14:08 +0000

    MVP

--- code/main.sh
+++ code/main.sh
@@ -36,34 +36,6 @@ fi
 GROUP_CONDITION_COLNAME="$1"
 SAMPLE_ID="$2"
 
-# count filtering
-MIN_NONZERO_COUNT_VAL="$3"
-MIN_NOSAMPLE_COUNT_VAL_TOTAL="$4"
-MIN_NOSAMPLE_COUNT_VAL_GROUP="$5"
-
-# diff expression
-COVARIATES_COLNAMES="$6"
-CONTRAST_COLNAME="$7"
-CONTRAST_TO_DO="$8"
-
-# diff expression filtering
-SIG_THRESHOLD="$9"
-FC_CUTOFF="${10}"
-CONTRAST_FILTER="${11}"
-CONTRAST_NAMES_FILTER="${12}"
-GROUPS_FILTER="${13}"
-GROUPS_NAMES_FILTER="${14}"
-
-# plot options
-DIFF_PLOT_TYPE="${15}"
-DIFF_LABEL_FONT_SIZE="${16}"
-DIFF_LABEL_DIST="${17}"
-DIFF_Y_EXPAN="${18}"
-DIFF_FILL_COLOURS="${19}"
-DIFF_BAR_WIDTH="${20}"
-DIFF_BAR_BORDER="${21}"
-DIFF_PLOT_FONTSIZE="${22}"
-
 set +a  
 
 
@@ -73,17 +45,6 @@ mkdir ../results/jsons/
 
 
 envsubst < json_args/args_create.json > ../results/jsons/args_create.rendered.json
-envsubst < json_args/args_clean.json > ../results/jsons/args_clean.rendered.json
-envsubst < json_args/args_filter.json > ../results/jsons/args_filter.rendered.json
-envsubst < json_args/args_norm.json > ../results/jsons/args_norm.rendered.json
-envsubst < json_args/args_diff.json > ../results/jsons/args_diff.rendered.json
-envsubst < json_args/args_diff_filter.json > ../results/jsons/args_diff_filter.rendered.json
-
 mosuite create_multiOmicDataSet_from_files --json=../results/jsons/args_create.rendered.json
-mosuite clean_raw_counts --json=../results/jsons/args_clean.rendered.json
-mosuite filter_counts --json=../results/jsons/args_filter.rendered.json
-mosuite normalize_counts --json=../results/jsons/args_norm.rendered.json
-mosuite diff_counts --json=../results/jsons/args_diff.rendered.json
-mosuite filter_diff --json=../results/jsons/args_diff_filter.rendered.json
 
 
